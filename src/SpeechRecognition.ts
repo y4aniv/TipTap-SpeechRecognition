7f1b7c7e87d7f290059bb4b7b26d4fdc7d0cf2c5
Yaniv <120222659+y4aniv@users.noreply.github.com>
2023-06-23 02:00:20 +0200

    v1.0.0-Alpha

---
+++ src/SpeechRecognition.ts
@@ -0,0 +1,60 @@
+import { Node } from '@tiptap/core'
+
+export interface SpeechRecognitionOptions {
+  lang: string, 
+}
+
+declare module '@tiptap/core' {
+  interface Commands<ReturnType> {
+    SpeechRecognition: {
+      startSpeechRecognition: () => ReturnType,
+      stopSpeechRecognition: () => ReturnType,
+    }
+  }
+}
+
+const SpeechRecognition = Node.create<SpeechRecognitionOptions>({
+  name: 'SpeechRecognition',
+
+  addOptions() {
+    return {
+      lang: 'fr-FR', 
+    }
+  },
+
+  addCommands() {
+    return {
+      startSpeechRecognition: () => ({ commands }) => {
+        const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition
+        this.recognition = new SpeechRecognition()
+
+        this.recognition.lang = this.options.lang
+        this.recognition.interimResults = false
+        this.recognition.maxAlternatives = 1
+        this.recognition.continuous = true
+
+        this.recognition.start()
+
+        this.recognition.finalTranscript = ''
+
+        let i = 0
+
+        this.recognition.onresult = (event) => {
+          var result = event.results[i][0].transcript
+          this.editor.commands.insertContent(result)
+          i++
+        }
+
+        return "RECOGNITION STARTED"
+      },
+
+      stopSpeechRecognition: () => ({ commands }) => {
+        this.recognition.stop()
+      }
+    }
+  },
+})
+
+export { SpeechRecognition }
+
+export default SpeechRecognition
