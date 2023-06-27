61541b57cfce99abb2b277cdaa2f4591c483417c
Yaniv <120222659+y4aniv@users.noreply.github.com>
2023-06-27 11:53:02 +0200

    v2.0.1-Alpha

--- src/TipTap-SpeechRecognition/src/SpeechRecognition.ts
+++ src/TipTap-SpeechRecognition/src/SpeechRecognition.ts
@@ -13,7 +13,14 @@ declare module '@tiptap/core' {
   }
 }
 
-const SpeechRecognition = Node.create<SpeechRecognitionOptions>({
+class SR_Node<O = any, S = any> extends Node<O, S> {
+  recognition: SpeechRecognition;
+  static create<O = any, S = any>(config?: any) {
+    return Node.create(config) as SR_Node<O, S>;
+  }
+};
+
+const SpeechRecognition = SR_Node.create<SpeechRecognitionOptions>({
   name: 'SpeechRecognition',
 
   addOptions() {
@@ -33,36 +40,50 @@ const SpeechRecognition = Node.create<SpeechRecognitionOptions>({
         this.recognition.maxAlternatives = 1
         this.recognition.continuous = true
 
-        this.recognition.start()
+        this.recognition.start();
 
         this.recognition.contentLength = this.editor.getText().length + 1
+        this.recognition.quoicoubeh = null
 
         this.recognition.onresult = (event) => {
 
           this.recognition.currentResult = ""
 
+          // Ajouter à la variable currentResult le contenu de la dernière phrase reconnue
           for (let i = event.resultIndex; i < event.results.length; i++) {
             this.recognition.currentResult += event.results[i][0].transcript
           }
+
+          // Supprimer la dernière phrase afficher (currentResult) dans l'éditeur
           this.editor.commands.deleteRange({
             from: this.recognition.contentLength,
             to: this.editor.getText().length + 1,
           })
-          this.editor.commands.insertContentAt(this.recognition.contentLength, this.recognition.currentResult)
+
+          // Ajouter la dernière phrase reconnue (currentResult) dans l'éditeur avec un style
+          this.editor.commands.insertContentAt(this.recognition.contentLength, `<code>${this.recognition.currentResult}</code>`)
+
+          // Si la dernière phrase reconnue est finale, supprimer la dernière phrase reconnue (currentResult) dans l'éditeur et réecrir la dernière phrase reconnue (currentResult) dans l'éditeur sans style
           if (event.results[event.results.length - 1].isFinal) {
+            this.editor.commands.deleteRange({
+              from: this.recognition.contentLength,
+              to: this.editor.getText().length + 1,
+            })
+            this.editor.commands.insertContentAt(this.recognition.contentLength, this.recognition.currentResult)
+            
+            // Redéfinir la variable contentLength en prenant en compte la dernière phrase reconnue
             this.recognition.contentLength += event.results[event.results.length - 1][0].transcript.length + 1
           }
         }
 
-        return "RECOGNITION STARTED"
+        return true
       },
 
       stopSpeechRecognition: () => ({ commands }) => {
-        console.log(this.recognition.results)
         this.recognition.stop()
         this.editor.commands.focus()
         this.recognition.lastResult = ''
-        return "RECOGNITION STOPPED"
+        return true
       }
     }
   },
